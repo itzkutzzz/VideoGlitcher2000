2486b62f1ab1d0ec9e7e461e8ccc14ccd89ff467
kawaii dude <kurt3daybann@gmail.com>
2025-03-27 02:06:13 -0400

    Update LICENSE.bat

--- LICENSE.bat
+++ LICENSE.bat
@@ -11,9 +11,9 @@ echo Share - copy and redistribute the material in any medium or format for any 
 echo Adapt - remix, transform, and build upon the material for any purpose, even commercially.
 echo The licensor cannot revoke these freedoms as long as you follow the license terms.
 echo Under the following terms:
-echo Attribution - You must give appropriate credit, provide a link to the license, and indicate if changes were made.
-echo You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+echo Attribution - You must give appropriate credit , provide a link to the license, and indicate if changes were made . You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
 echo No additional restrictions - You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
 pause
+color 0F
 cls
 exit
